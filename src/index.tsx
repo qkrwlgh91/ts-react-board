9637afffc438064d8fe51e40dfd87c417be0076d
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-11-02 15:40:16 +0900

    scss standard set up

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'scss/tsReactBoard.scss';
 
 ReactDOM.render(
   <Router>
