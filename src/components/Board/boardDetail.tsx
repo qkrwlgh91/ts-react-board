89d1f76613bcc7e66950e9af1d4aa5f1ea893c98
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-11-05 15:52:07 +0900

    board detail scss standard set up

--- src/components/Board/boardDetail.tsx
+++ src/components/Board/boardDetail.tsx
@@ -4,7 +4,20 @@ import Command from './command';
 function boardDetail() {
     return (
         <div className="boardBox__detail">
-            didididdi
+            <div className="boardBox__detail--info">
+                <div className="boardBox__detail--info-img">
+                    이미지
+                </div>
+                <div className="boardBox__detail--info-cont">
+                    <h1>게시글 첫번째 항목입니다. 제</h1>
+                    <p>qkrwlgh</p>
+                    <p>2020-11-03</p>
+                    <p>조회수 10</p>
+                </div>
+            </div>
+            <div className="boardBox__detail--content">
+                dfjkghfds;ughsd;fguhsdfguhds
+            </div>
             <Command />
         </div>
     )
