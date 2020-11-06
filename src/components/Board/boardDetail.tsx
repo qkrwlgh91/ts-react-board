c617f5237c245e037e59a625f745febf2c891c7b
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-11-06 09:20:00 +0900

    board list scss

--- src/components/Board/boardDetail.tsx
+++ src/components/Board/boardDetail.tsx
@@ -9,7 +9,7 @@ function boardDetail() {
                     이미지
                 </div>
                 <div className="boardBox__detail--info-cont">
-                    <h1>게시글 첫번째 항목입니다. 제</h1>
+                    <h1>게시글 첫번째 항목입니다. 제목은 두줄까지 보여집니다.</h1>
                     <p>qkrwlgh</p>
                     <p>2020-11-03</p>
                     <p>조회수 10</p>
--- src/components/Board/boardList.tsx
+++ src/components/Board/boardList.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
 
 function boardList() {
     return (
         <div className="boardBox__list">
-            sadfjkdahg;ds
+            <div className="board__search">
+                <select name="" id="">
+                    <option value="">작성자</option>
+                    <option value="">글제목</option>
+                </select>
+                <input type="text"/>
+                <button>검색</button>
+            </div>
+            <div className="board__list">
+                <div className="board__list--header">
+                    <h2>qkrwlgh</h2>
+                    <h2>2020-11-03</h2>
+                </div>
+                <h1>게시글 첫번째 항목입니다. 두번째 칸까지 타이틀이 보입니다. 람쥐</h1>
+                <div className="board__list--footer">
+                    <p>조회수 10</p>
+                    <p>댓글 3</p>
+                </div>
+            </div>
+            <div className="board__wbtn">
+                <button className="board__wbtn-btn">글작성</button>
+            </div>
+            <div  className="board__paging">
+                페이징
+            </div>
         </div>
     )
 }
 
-export default boardList
+export default boardList;
